1273f05ea187f86845a4fa144bd52c98902aad16
Bruno Mello <bruno.barros.mello@gmail.com>
2025-05-29 18:24:46 -0300

    draw fps

--- src/main.go
+++ src/main.go
@@ -1,6 +1,10 @@
 package main
 
-import rl "github.com/gen2brain/raylib-go/raylib"
+import (
+	"fmt"
+
+	rl "github.com/gen2brain/raylib-go/raylib"
+)
 
 func main() {
 	screenWidth, screenHeight := int32(1200), int32(675)
@@ -34,6 +38,8 @@ func main() {
 
 		rl.EndMode3D()
 
+		drawFPS(screenWidth)
+
 		rl.EndDrawing()
 	}
 
@@ -53,4 +59,19 @@ func buildChunks() []*Chunk {
 	}
 
 	return chunks
+}
+
+func drawFPS(screenW int32) {
+	fps := rl.GetFPS()
+	text := fmt.Sprintf("%d FPS", fps)
+	fontSize := int32(20)
+
+	// Measure the width of the text so we can right-align it
+	textWidth := rl.MeasureText(text, fontSize)
+
+	// draw it with a little padding from the edges:
+	x := screenW - textWidth - 10
+	y := int32(10)
+
+	rl.DrawText(text, x, y, fontSize, rl.Black)
 }
