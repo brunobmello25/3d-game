5b9f04edf750fd2a02a3a109e2fece6504c12371
Bruno Mello <bruno.barros.mello@gmail.com>
2025-06-01 21:46:52 -0300

    use basic raylib camera movement for now

--- src/player/player.go
+++ src/player/player.go
@@ -1,116 +1,73 @@
 package player
 
 import (
-	"math"
-
 	rl "github.com/gen2brain/raylib-go/raylib"
 )
 
 const (
-	PLAYER_SPEED      = 0.18
+	CAMERA_SPEED      = 0.18
 	MOUSE_SENSITIVITY = 0.006
 )
 
 type Player struct {
 	Camera   rl.Camera3D
 	Position rl.Vector3
-
-	rotation rl.Vector2
 }
 
 func NewPlayer() *Player {
+	position := rl.NewVector3(0, 0, 0)
 	return &Player{
 		Camera: rl.NewCamera3D(
-			rl.NewVector3(0, 0, 0), // pos
+			position,
 			rl.NewVector3(1, 0, 0), // target
 			rl.NewVector3(0, 1, 0), // up
 			60,
 			rl.CameraPerspective,
 		),
-		Position: rl.NewVector3(0, 0, 0),
-		rotation: rl.NewVector2(0, 0),
+		Position: position,
 	}
 }
 
+// TODO: should probably handle camera movement and rotation
+// manually instead of using raylib's camera functions.
+// Ideally, we want to update player position and rotation
+// based on user input, and then just update camera values
+// accordingly.
 func (p *Player) Update() {
-	mouseDelta := rl.GetMouseDelta()
+	var mousePositionDelta = rl.GetMouseDelta()
 
-	p.rotation.X -= float32(mouseDelta.X) * MOUSE_SENSITIVITY
-	p.rotation.Y -= float32(mouseDelta.Y) * MOUSE_SENSITIVITY
+	var moveInWorldPlane uint8
+	moveInWorldPlane = 1
 
-	// Clamp vertical rotation
-	if p.rotation.Y < -89 {
-		p.rotation.Y = -89
-	} else if p.rotation.Y > 89 {
-		p.rotation.Y = 89
-	}
+	var rotateAroundTarget uint8
 
-	// Calculate forward and right vectors
-	forward := rl.Vector3{
-		X: float32(math.Cos(float64(rl.Deg2rad * p.rotation.X))),
-		Y: 0,
-		Z: float32(math.Sin(float64(rl.Deg2rad * p.rotation.X))),
-	}
-	right := rl.Vector3{
-		X: float32(math.Cos(float64(rl.Deg2rad * (p.rotation.X + 90)))),
-		Y: 0,
-		Z: float32(math.Sin(float64(rl.Deg2rad * (p.rotation.X + 90)))),
-	}
+	var lockView uint8
+	lockView = 1
 
-	// Handle keyboard input
+	var rotateUp uint8
+
+	rl.CameraYaw(&p.Camera, -mousePositionDelta.X*MOUSE_SENSITIVITY, rotateAroundTarget)
+	rl.CameraPitch(&p.Camera, -mousePositionDelta.Y*MOUSE_SENSITIVITY, lockView, rotateAroundTarget, rotateUp)
+
+	// Keyboard support
 	if rl.IsKeyDown(rl.KeyW) {
-		p.Position = rl.Vector3Add(p.Position, rl.Vector3Scale(forward, PLAYER_SPEED))
-	}
-	if rl.IsKeyDown(rl.KeyS) {
-		p.Position = rl.Vector3Subtract(p.Position, rl.Vector3Scale(forward, PLAYER_SPEED))
+		rl.CameraMoveForward(&p.Camera, CAMERA_SPEED, moveInWorldPlane)
 	}
 	if rl.IsKeyDown(rl.KeyA) {
-		p.Position = rl.Vector3Subtract(p.Position, rl.Vector3Scale(right, PLAYER_SPEED))
+		rl.CameraMoveRight(&p.Camera, -CAMERA_SPEED, moveInWorldPlane)
+	}
+	if rl.IsKeyDown(rl.KeyS) {
+		rl.CameraMoveForward(&p.Camera, -CAMERA_SPEED, moveInWorldPlane)
 	}
 	if rl.IsKeyDown(rl.KeyD) {
-		p.Position = rl.Vector3Add(p.Position, rl.Vector3Scale(right, PLAYER_SPEED))
+		rl.CameraMoveRight(&p.Camera, CAMERA_SPEED, moveInWorldPlane)
+	}
+	if rl.IsKeyDown(rl.KeySpace) {
+		rl.CameraMoveUp(&p.Camera, CAMERA_SPEED)
+	}
+	if rl.IsKeyDown(rl.KeyLeftShift) {
+		rl.CameraMoveUp(&p.Camera, -CAMERA_SPEED)
 	}
 
-	// Update camera position and target
-	p.Camera.Position = p.Position
-	p.Camera.Target = rl.Vector3Add(p.Position, forward)
+	p.Position = p.Camera.Position
 }
-
-// TODO: remove this after refactor is done
-// func UpdateCamera(camera *rl.Camera) {
-// 	var mousePositionDelta = rl.GetMouseDelta()
-//
-// 	var moveInWorldPlane uint8
-// 	moveInWorldPlane = 1
-//
-// 	var rotateAroundTarget uint8
-//
-// 	var lockView uint8
-// 	lockView = 1
-//
-// 	var rotateUp uint8
-//
-// 	rl.CameraYaw(camera, -mousePositionDelta.X*MOUSE_SENSITIVITY, rotateAroundTarget)
-// 	rl.CameraPitch(camera, -mousePositionDelta.Y*MOUSE_SENSITIVITY, lockView, rotateAroundTarget, rotateUp)
-//
-// 	// Keyboard support
-// 	if rl.IsKeyDown(rl.KeyW) {
-// 		rl.CameraMoveForward(camera, CAMERA_SPEED, moveInWorldPlane)
-// 	}
-// 	if rl.IsKeyDown(rl.KeyA) {
-// 		rl.CameraMoveRight(camera, -CAMERA_SPEED, moveInWorldPlane)
-// 	}
-// 	if rl.IsKeyDown(rl.KeyS) {
-// 		rl.CameraMoveForward(camera, -CAMERA_SPEED, moveInWorldPlane)
-// 	}
-// 	if rl.IsKeyDown(rl.KeyD) {
-// 		rl.CameraMoveRight(camera, CAMERA_SPEED, moveInWorldPlane)
-// 	}
-// 	if rl.IsKeyDown(rl.KeySpace) {
-// 		rl.CameraMoveUp(camera, CAMERA_SPEED)
-// 	}
-// 	if rl.IsKeyDown(rl.KeyLeftShift) {
-// 		rl.CameraMoveUp(camera, -CAMERA_SPEED)
-// 	}
-//
-// }
